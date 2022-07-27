b7698c800b7dff26535100c7a0f0c661f1e9b922
Yeray Gonzalez bartolome <ygonzale@c3r6s5.42urduliz.com>
2022-07-27 15:30:35 +0200

    historial cocn rangos de feccha

--- chrome_history.py
+++ chrome_history.py
@@ -1,26 +1,28 @@
+import datetime
+import sqlite3
+import os
+import sys
 
-""" import sqlite3
+year_min = int(sys.argv[1])
+month_min = int(sys.argv[2])
+day_min = int(sys.argv[3])
+year_max = int(sys.argv[4])
+month_max = int(sys.argv[5])
+day_max = int(sys.argv[6])
 
-#con = sqlite3.connect('C:\Users\$user\AppData\Local\Google\Chrome\User Data\Default\History')
-con = sqlite3.connect('/Users/ygonzale/Library/Application Support/Google/Chrome/Default')
+ts_min= (datetime.datetime(year_min, month_min, day_min, 0, 0) - datetime.datetime(1601,1,1)).total_seconds()
+ts_max= (datetime.datetime(year_max, month_max, day_max, 0, 0) - datetime.datetime(1601,1,1)).total_seconds()
 
-#/Users/ygonzale/Library/Application Support/Google/Chrome/Default
+ts_min = int(ts_min) * 1000000
+ts_max = int(ts_max) * 1000000
 
+con = sqlite3.connect('/Users/ygonzale/Library/Application Support/Google/Chrome/Default/History')
 c = con.cursor()
 
-c.execute("select url, title, visit_count, last_visit_time from urls")
+c.execute("select * from urls where last_visit_time between '"+ str(ts_min) +"' and '"+ str(ts_max)  +"'")
 results = c.fetchall()
 
 for r in results:
 	print(r)
 
-c.close() """
-
-archivo = open("/Users/ygonzale/Library/Application Support/Google/Chrome/Default/History")
-
-linea = archivo.readline()
-while linea != '':
-
-	linea = archivo.readline()
-	#linea = linea.split('')
-	print(linea)
+c.close()
