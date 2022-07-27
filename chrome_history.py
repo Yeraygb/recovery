0e2b4aa322a0acdf04365a659d3815c002a58472
Yeray Gonzalez bartolome <ygonzale@c3r6s5.42urduliz.com>
2022-07-27 15:48:08 +0200

    historial cocn rangos de feccha

--- chrome_history.py
+++ chrome_history.py
@@ -3,6 +3,8 @@ import sqlite3
 import os
 import sys
 
+user = os.environ.get("USER")
+
 year_min = int(sys.argv[1])
 month_min = int(sys.argv[2])
 day_min = int(sys.argv[3])
@@ -16,7 +18,7 @@ ts_max= (datetime.datetime(year_max, month_max, day_max, 0, 0) - datetime.dateti
 ts_min = int(ts_min) * 1000000
 ts_max = int(ts_max) * 1000000
 
-con = sqlite3.connect('/Users/ygonzale/Library/Application Support/Google/Chrome/Default/History')
+con = sqlite3.connect('/Users/'+user+'/Library/Application Support/Google/Chrome/Default/History')
 c = con.cursor()
 
 c.execute("select * from urls where last_visit_time between '"+ str(ts_min) +"' and '"+ str(ts_max)  +"'")
