d42a564a5419c0926e5c66440d63964ca53d5d92
Yeray Gonzalez bartolome <ygonzale@c3r6s5.42urduliz.com>
2022-07-28 12:12:29 +0200

    24h less 1 get

--- chrome_history.py
+++ chrome_history.py
@@ -79,9 +79,28 @@ else:
 	year_less1 = get_yearl_less1()
 	month_less1 = get_month_less1()
 	day_less1 = get_day_less1()
-	print(year)
-	print(mes)
-	print(day)
-	print(year_less1)
-	print(month_less1)
-	print(day_less1)
+	#print(year)
+	#print(mes)
+	#print(day)
+	#print(year_less1)
+	#print(month_less1)
+	#print(day_less1)
+	ts_min = (datetime.datetime(year_less1, month_less1, day_less1, 0, 0) - datetime.datetime(1601,1,1)).total_seconds()
+	ts_max = (datetime.datetime(year, mes, day, 0, 0) - datetime.datetime(1601,1,1)).total_seconds()
+
+	ts_min = int(ts_min) * 1000000
+	ts_max = int(ts_max) * 1000000
+
+	#Ruta para Mac
+	con = sqlite3.connect('/Users/'+user+'/Library/Application Support/Google/Chrome/Default/History')
+	#Ruta para Windows
+	#con = sqlite3.connect('/Users/IEUser/AppData/Local/Google/Chrome/User Data/Default/History')
+	c = con.cursor()
+
+	c.execute("select * from urls where last_visit_time between '"+ str(ts_min) +"' and '"+ str(ts_max)  +"'")
+	results = c.fetchall()
+
+	for r in results:
+		print(r)
+
+	c.close()
